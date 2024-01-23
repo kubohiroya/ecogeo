297ee329e79a43f03cc0716dfe594654e2cd51fc
Hiroya Kubo <hiroya@cuc.ac.jp>
2024-01-23 09:59:38 +0000

    update dependencies

--- src/app/components/SessionPanel/MapPanel/pixi/FontStyles.ts
+++ src/app/components/SessionPanel/MapPanel/pixi/FontStyles.ts
@@ -1,19 +1,19 @@
-import * as PIXI from 'pixi.js';
+import { TextStyle } from 'pixi.js';
 
-export const StyleSize14 = new PIXI.TextStyle({
+export const StyleSize14 = new TextStyle({
   fontSize: 14,
   fontFamily: '"Source Sans Pro", Helvetica, sans-serif',
   fill: '#333333',
   align: 'center',
 });
-export const StyleSize14Focused = new PIXI.TextStyle({
+export const StyleSize14Focused = new TextStyle({
   fontSize: 14,
   fontFamily: '"Source Sans Pro", Helvetica, sans-serif',
   fill: '#dd0000',
   align: 'center',
 });
 
-export const StyleSize11 = new PIXI.TextStyle({
+export const StyleSize11 = new TextStyle({
   fontSize: 11,
   fontFamily: '"Source Sans Pro", Helvetica, sans-serif',
   align: 'center',
@@ -21,7 +21,7 @@ export const StyleSize11 = new PIXI.TextStyle({
 });
 
 export const fontStyle = (style: Object) =>
-  new PIXI.TextStyle({
+  new TextStyle({
     fontSize: 11,
     fontFamily: '"Source Sans Pro", Helvetica, sans-serif',
     fill: '#333333',
